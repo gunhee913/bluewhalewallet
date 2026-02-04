4ff49550291317f6dcd4067c651e1b6bbd7537de
gunhee913 <52226425+gunhee913@users.noreply.github.com>
2026-02-04 22:19:19 +0900

    Add new SHELL CLUB member (???)

--- src/constants/shell-club.ts
+++ src/constants/shell-club.ts
@@ -7,6 +7,7 @@ export const SHELL_CLUB_MEMBERS: { name: string; address: string }[] = [
   { name: '홀더(2)', address: '0x6f46eab401d6a708a57efaed52190c274604ac9a' },
   { name: '홀더(3)', address: '0xdae0c3d31bbbf2582d16b68079f0be6d9165fc7a' },
   { name: '홀더(4)', address: '0x40688699Cd9f67418e5A5c6bEA9d51Ade01FDb6a' },
+  { name: '홀더(5)', address: '0x2ed38f3e036c6593f64fd4c7c679790cbb6e73af' },
 ];
 
 /**

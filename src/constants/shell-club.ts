4085669bdf9b7b08cf4b485a46c074de3533ee25
gunhee913 <52226425+gunhee913@users.noreply.github.com>
2026-02-05 21:22:18 +0900

    zxcjhaskjhjkczxc

--- src/constants/shell-club.ts
+++ src/constants/shell-club.ts
@@ -8,6 +8,9 @@ export const SHELL_CLUB_MEMBERS: { name: string; address: string }[] = [
   { name: '홀더(3)', address: '0xdae0c3d31bbbf2582d16b68079f0be6d9165fc7a' },
   { name: '홀더(4)', address: '0x40688699Cd9f67418e5A5c6bEA9d51Ade01FDb6a' },
   { name: '홀더(5)', address: '0x2ed38f3e036c6593f64fd4c7c679790cbb6e73af' },
+  { name: '홀더(6)', address: '0x01915b8577c7f04973e465dcfd8942267d910912' },
+  { name: '홀더(7)', address: '0x305fd254e4b10d4dbdbf7a143cf3b6dad0fe9695' },
+  { name: '홀더(8)', address: '0x8295655e0686f156f48896f61a51ef17daa6e41f' },
 ];
 
 /**
